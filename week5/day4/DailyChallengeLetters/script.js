84461b4f119d92fdedea113a6290350c6e12008a
ortori <ortori@gmail.com>
2022-04-13 23:46:27 +0300

    .

--- week5/day4/DailyChallengeLetters/script.js
+++ week5/day4/DailyChallengeLetters/script.js
@@ -1,8 +1,8 @@
 "use strict";
 const input = document.querySelector("input");
 const regex = /[a-zA-Z | ]/;
-input.addEventListener("keyup", function (e) {
+input.addEventListener("keydown", function (e) {
   if (!regex.test(e.key)) {
-    input.value = input.value.slice(0, input.value.indexOf(e.key));
+    e.preventDefault();
   }
 });
