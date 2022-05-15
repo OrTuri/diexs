5abf43355ffd8a6ef00f482fc646967fe0f56160
ortori <ortori@gmail.com>
2022-05-15 17:28:43 +0300

    ,

---
+++ week8/DailyChallengeDates/script.js
@@ -0,0 +1,11 @@
+"use strict";
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hour = date.getHours().toString().padStart(2, "0");
+  const minute = date.getMinutes().toString().padStart(2, "0");
+  const second = date.getSeconds().toString().padStart(2, "0");
+  return year + day + month + hour + minute + second;
+};
+formatDate(new Date(1992, 2, 30, 23, 59, 2));
